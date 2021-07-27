721a3902352ab8ddbf4022a6e6e794b7ed555546
jgocm <jgocm@cin.ufpe.br>
2021-07-27 19:35:26 -0300

    add mlp

---
+++ mlp.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import argmax
+import pandas as pd
+import os.path
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RepeatedKFold
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn import svm
+import data_analysis
+
+# IMPORT DATASET WITH PANDAS
+DF_PATH = os.path.dirname(__file__) + "/glass.csv"
+df = pd.read_csv(DF_PATH, encoding="utf-8")
+X = np.array(df.iloc[:,  :-1])
+y = np.array(df.iloc[:,-1:  ])
+
+# DATA EXPLORATORY ANALYSIS AND PREPROCESSING
+#data_analysis.run(df, False)
+
+df_drop = df.drop(columns="RI")
+#print(df_drop.info())
+
+# ADJUST HYPERPARAMETERS WITH VALIDATION SET
+# SPLIT DATASET
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=42)
+
+# STANDARDIZATION
+from sklearn.preprocessing import StandardScaler
+scaler1 = StandardScaler()  
+# Fit only on training data
+scaler1.fit(X_train)  
+X_train_std = scaler1.transform(X_train)  
+# apply same transformation to test data
+X_test_std = scaler1.transform(X_test)
+
+# NORMALIZATION
+from sklearn.preprocessing import Normalizer
+scaler2 = Normalizer()  
+# Fit only on training data
+scaler2.fit(X_train)  
+X_train_norm = scaler2.transform(X_train)  
+# apply same transformation to test data
+X_test_norm = scaler2.transform(X_test)
+
+# Feature scaler
+scaler = 'std'
+
+if (scaler == 'std'): 
+    X_train = X_train_std
+    X_test = X_test_std
+if (scaler == 'norm'): 
+    X_train = X_train_norm
+    X_test = X_test_norm
+
+# GENERATE CLASSIFIER AND FIND BEST HYPERPARAMETERS
+mlp_gs = MLPClassifier(max_iter=10000)
+parameter_space = {
+    'hidden_layer_sizes': [(10,30,10),(20,)],
+    'activation': ['identity', 'logistic', 'tanh', 'relu'],
+    'solver': ['lbfgs', 'sgd', 'adam'],
+    'alpha': [0.0001, 0.05],
+    'learning_rate': ['constant', 'invscaling', 'adaptive'],
+}
+
+clf = RandomizedSearchCV(mlp_gs, parameter_space, n_iter=30, n_jobs=-1, cv=5)
+clf.fit(X_train, y_train) # X is train samples and y is the corresponding labels
+
+print('Best parameters found:\n', clf.best_params_)
+
+means = clf.cv_results_['mean_test_score']
+stds = clf.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+   print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+
+y_true, y_pred = y_test , clf.predict(X_test)
+from sklearn.metrics import classification_report
+print('Results on the test set:')
+print(classification_report(y_true, y_pred))
+
+# GENERATE MODELS
+
+
+
+# 10 REPETITIONS OF 5-FOLD CROSS VALIDATION
+n_splits = 5
+n_repeats = 10
+
+# TEST MODEL
+scores = []
+rkf = RepeatedKFold(n_splits, n_repeats)
+for train_index, test_index in rkf.split(X):
+    X_train, X_test = X[train_index], X[test_index]
+    y_train, y_test = y[train_index], y[test_index]
+    #scores.append(clf.fit(X_train, y_train).score(X_test, y_test))
+#print(scores)
--- svm.py
+++ svm.py
@@ -33,43 +33,54 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_
 # BEST CLASSIFIER: best_clf = [accuracy C]
 # FOR POLYNOMIAL: best_poly = [accuracy C degree]
 gamma = "auto"
-lin_scores = [svm.SVC(kernel="linear", gamma=gamma, C=c).fit(X_train, y_train).score(X_test, y_test) for c in range(1,10)]
+lin_scores = [svm.SVC(kernel="linear", gamma=gamma, C=c).fit(X_train, y_train).score(X_test, y_test) for c in range(1,200)]
 best_lin = [max(lin_scores), argmax(lin_scores)+1]
-rbf_scores = [svm.SVC(kernel="rbf", gamma=gamma, C=c).fit(X_train, y_train).score(X_test, y_test) for c in range(1,10)]
+rbf_scores = [svm.SVC(kernel="rbf", gamma=gamma, C=c).fit(X_train, y_train).score(X_test, y_test) for c in range(1,200)]
 best_rbf = [max(rbf_scores), argmax(rbf_scores)+1]
-sgd_scores = [svm.SVC(kernel="sigmoid", gamma=gamma, C=c).fit(X_train, y_train).score(X_test, y_test) for c in range(1,10)]
+sgd_scores = [svm.SVC(kernel="sigmoid", gamma=gamma, C=c).fit(X_train, y_train).score(X_test, y_test) for c in range(1,200)]
 best_sgd = [max(sgd_scores), argmax(sgd_scores)+1]
 poly_scores = []
-for degree in range (2,20):
-    current_scores=([svm.SVC(kernel="poly", gamma=gamma, degree=degree, C=c).fit(X_train, y_train).score(X_test, y_test) for c in range(1,10)])
+for degree in range (3):
+    current_scores=([svm.SVC(kernel="poly", gamma=gamma, degree=degree, C=c).fit(X_train, y_train).score(X_test, y_test) for c in range(1,200)])
     poly_scores.append([max(current_scores),argmax(current_scores)+1, degree])
 poly_scores=np.array(poly_scores)
-x_plot=poly_scores[:,2]
-scores=poly_scores[:,0]
-y_plot=scores
-best_poly=poly_scores[argmax(scores)]
+poly_scores = [svm.SVC(kernel="poly", gamma=gamma, degree=3, C=c).fit(X_train, y_train).score(X_test, y_test) for c in range(1,200)]
+best_poly=[max(poly_scores), argmax(poly_scores)+1]
+#x_plot=poly_scores[:,2]
+#y_plot=scores
+#plt.ylabel("accuracy")
+#plt.xlabel("C")
+#plt.title("Polynomial Kernels")
+#plt.grid(1)
+#plt.xticks(x_plot)
+#plt.plot(x_plot,y=y_plot) 
+x_plot=range(1,200)
 plt.ylabel("accuracy")
-plt.xlabel("degree")
-plt.title("Polynomial Kernels")
-plt.grid(1)
-plt.xticks(x_plot)
-plt.plot(x_plot,y_plot) 
+plt.xlabel("C")
+plt.title("C validation")
+plt.plot(x_plot,lin_scores,'r', label="linear")
+plt.plot(x_plot,rbf_scores,'b', label="rbf")
+plt.plot(x_plot,sgd_scores,'g', label="sigmoid")
+plt.plot(x_plot,poly_scores,'y', label="polynomial")
+plt.legend()
 plt.show()
 
 # GENERATE MODELS
-#C = best_lin[1]
-C=3
+C = best_lin[1]
+#C=147
 svm_lin = svm.SVC(kernel="linear", gamma=gamma, C=C)
-#C = best_rbf[1]
-C=7
+C = best_rbf[1]
+#C=190
 svm_rbf = svm.SVC(kernel="rbf", gamma=gamma, C=C)
-#C = best_sgd[1]
-C=1
+C = best_sgd[1]
+#C=171
 svm_sgd = svm.SVC(kernel="sigmoid", gamma=gamma, C=C)
-#C = best_poly[1]
-C=1
-degree = 5
+C = best_poly[1]
+#C=159
+degree = 3
 svm_poly = svm.SVC(kernel="poly", gamma=gamma, C=C, degree=degree)
+
+val_scores=[clf.fit(X_train, y_train).score(X_test, y_test) for clf in (svm_lin, svm_rbf, svm_sgd, svm_poly)]
 
 # 10 REPETITIONS OF 5-FOLD CROSS VALIDATION
 n_splits = 5
@@ -82,9 +93,28 @@ for train_index, test_index in rkf.split(X):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
     scores.append([clf.fit(X_train, y_train).score(X_test, y_test) for clf in (svm_lin, svm_rbf, svm_sgd, svm_poly)])
-print(scores)
-lin_scores=np.array(scores[:,0])
-rbf_scores=np.array(scores[:,1])
-sgd_scores=np.array(scores[:,2])
-svm_scores=np.array(scores[:,3])
-print(svm_lin.C, svm_rbf.C, svm_sgd.C, svm_poly.C)
+# print(scores)
+# LINEAR KERNEL
+scores = np.array(scores)
+lin_scores=scores[:,0]
+lin_mean=np.average(lin_scores)
+lin_var=np.var(lin_scores)
+print("lin:",lin_mean, lin_var)
+#
+rbf_scores=scores[:,1]
+rbf_mean=np.average(rbf_scores)
+rbf_var=np.var(rbf_scores)
+print("rbf:",rbf_mean, rbf_var)
+#
+sgd_scores=scores[:,2]
+sgd_mean=np.average(sgd_scores)
+sgd_var=np.var(sgd_scores)
+print("sgd:",sgd_mean, sgd_var)
+#
+poly_scores=scores[:,3]
+poly_mean=np.average(poly_scores)
+poly_var=np.var(poly_scores)
+print("poly:",poly_mean, poly_var)
+
+print("validation scores: ", val_scores)
+print("C's: ",svm_lin.C, svm_rbf.C, svm_sgd.C, svm_poly.C)
